c1780e362a574f287d8a1c07e077f8b765446bda
Sandrine206 <sandrinetech@outlook.com>
2025-05-13 14:08:04 +0000

    DOM manipulation fixed
    A contact form added

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
-// get and store a ref to forms
-// get and store ref to input
-// return values from input to console
+const form = document.getElementById('contactForm')
+const displayBox = document.getElementById('formDataDisplay')
 
-const form = document.querySelector('form')
-
-function handleSubmit(e) {
-  //disables default form submission
+form.addEventListener('submit', function (e) {
   e.preventDefault()
 
-  // returns various values from input
-  const firstName = document.querySelector('#firstName').value
-  const lastName = document.querySelector('#lastName').value
-  const email = document.querySelector('#email').value
-  const age = document.querySelector('#age').value
-  const resultULView = document.querySelector('#result-view')
-  const ageErrorSpan = document.querySelector('#age-error-span')
+  // Collect form data
+  const firstName = document.getElementById('firstName').value
+  const secondName = document.getElementById('secondName').value
+  const email = document.getElementById('email').value
+  const message = document.getElementById('message').value
 
-  if (age < 22) {
-    alert('You must be 22 years or older to fill this form')
-    ageErrorSpan.innerHTML = `<p style="color red;">${errorMsg}</p>`
-  }
+  // Create a container to display the form data
+  const submittedData = document.createElement('div')
+  submittedData.classList.add('submitted-data')
 
-  //store data in an object
-  const formData = {
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    age: age,
-  }
-  console.log(formData)
+  // Add content with DOM manipulation
+  submittedData.innerHTML = `
+      <h3>Submitted Info:</h3>
+      <p><strong>First Name:</strong> ${firstName}</p>
+      <p><strong>Second Name:</strong> ${secondName}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Message:</strong> ${message}</p>
+    `
 
-  const firstString = 'Coding seems difficult'
-  const lastString = ' is but i will get it'
-  const resultString = firstString + 'it' + lastString
-  const resultString2 = `${firstString} it ${lastString} `
+  // Append or replace the display box content
+  displayBox.innerHTML = '' // Clear previous output
+  displayBox.appendChild(submittedData)
 
-  console.log(resultString2)
-
-  //update our DOM via DOM manipulation
-  resultULView.innerHTML = `<li>first Name: ${formData.firstName}</li>
-  <li> lastName : ${formData.lastName}</li>
-  <li> email: ${formData.email}</li>
-  <li> age : ${formData.age}</li> `
-}
-
-form.addEventListener('submit', handleSubmit)
+  // Reset form fields
+  form.reset()
+})
